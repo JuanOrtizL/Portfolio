902760fec8c9350741ae8c1be28c9998bd99cc0b
Juan Ortiz <juanorle2014@gmail.com>
2024-03-26 22:34:36 -0500

    Responsive

--- JS/script.js
+++ JS/script.js
@@ -8,3 +8,31 @@ menuIcon.onclick=()=>{
     nav.classList.toggle('active');
 
 }
+
+/*Scroll reveal*/ 
+
+ScrollReveal({
+    reset:true,
+    distance:'80px',
+    duration: 2000,
+    delay: 200
+
+});
+
+ScrollReveal().reveal('.home-content, .heading ', { origin:'left' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin:'bottom' });
+ScrollReveal().reveal('.home-content h1', { origin:'right' });
+ScrollReveal().reveal('.about-img', { origin:'top' });
+
+/* Typed JS*/
+const typed= new Typed('.multiple-text',{
+    strings:['Backend Developer', 'FullStack Developer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop:true,
+    showCursor:true,
+    cursorChar:'×'
+}) 
+
+
